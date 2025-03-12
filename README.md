6d366effe0cf1b15c17cf6e33324a2a729f0cabb
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-03-12 15:28:04 +0200

    collecting measurements

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # LeafCare Pot FirmWare
 
-### Current state: Skeleton
+### Current state: Measuring
 
-At this moment, the firmware implements lifecycle skeleton:
+At this moment, the firmware implements lifecycle skeleton with collecting measurements:
 1. ESP checks wakeup reason (timer/button)
 2. If the wakeup is caused by the button - LED turns on
 3. If the button is held long enough - ESP switches to pairing mode, or in default mode otherwise
-4. ESP goes into the deep sleep until a certain time or the button is pressed
+4. **Default mode**
+4.1. Collecting all measurements
+4.2. Printing results in Serial
+5. **Pairing mode**
+5.1. Serial Dummy
+6. ESP goes into the deep sleep until a certain time or the button is pressed
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,4 +1,7 @@
-// UTILS
+#include <Wire.h>
+#include <SHT2x.h>
+
+// UTILITY DEFINES
 #define MILLIS_TO_MICROS_FACTOR 1000
 #define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)
 
@@ -12,9 +15,55 @@
 #define USER_BUTTON_PIN 25 // User Interface Button Pin
 #define USER_LED_PIN 15    // User Interface LED Pin
 
-// GLOBALS
-RTC_DATA_ATTR int wakeUpCount = 0;
+// LIGHT
+#define LIGHT_PIN1 32 // Light Sensor 1 Pins
+#define LIGHT_PIN2 35
+#define LIGHT_PIN3 34
+#define LIGHT_MIN 0 // Light Sensor Value Range
+#define LIGHT_MAX 4096
 
+// MOISTURE
+#define MOISTURE_SENSOR_PIN 33 // Moisture Sensor Pin
+#define MOISTURE_MIN 3015      // Moisture Sensor Value Range
+#define MOISTURE_MAX 1650
+
+// WATER
+#define WATER_L1_PIN 13 // Water Level Pins
+#define WATER_L2_PIN 12
+#define WATER_L3_PIN 14
+#define WATER_L4_PIN 27
+#define WATER_L1_VALUE 25 // Water Level Values
+#define WATER_L2_VALUE 50
+#define WATER_L3_VALUE 75
+#define WATER_L4_VALUE 100
+#define WATER_SENSOR_PIN 39    // Water Sensor Pin
+#define WATER_THRESHOLD 1      // Water Sensor Value Threshold
+#define WATER_READING_TIME 100 // Time (ms, >= 2) for each level to be powered and measured
+
+// GLOBALS
+RTC_DATA_ATTR int timerWakeUpCount = 0;
+int buttonWakeUp = 0;
+SHT2x sht;
+
+// TYPES
+struct HumAndTemp
+{
+  float humidity;
+  float temperature;
+};
+
+struct Light
+{
+  int max; // Maximum value
+  int l1;  // Individual values in percents
+  int l2;
+  int l3;
+  int l1Raw; // Raws are values without mapping to percents
+  int l2Raw;
+  int l3Raw;
+};
+
+// UTILITIES
 int isButtonHeld(int pin, int time)
 {
   int start = millis();
@@ -36,10 +85,125 @@ int isButtonHeld(int pin, int time)
   }
 }
 
+// MEASURING
+HumAndTemp readHumAndTemp()
+{
+  struct HumAndTemp values;
+  sht.read();
+  values.humidity = constrain(sht.getHumidity(), 0, 100);
+  values.temperature = sht.getTemperature();
+  return values;
+}
+
+Light readLight()
+{
+  struct Light values;
+  values.l1Raw = analogRead(LIGHT_PIN1);
+  values.l2Raw = analogRead(LIGHT_PIN2);
+  values.l3Raw = analogRead(LIGHT_PIN3);
+  values.l1 = constrain(map(values.l1Raw, LIGHT_MIN, LIGHT_MAX, 0, 100), 0, 100);
+  values.l2 = constrain(map(values.l2Raw, LIGHT_MIN, LIGHT_MAX, 0, 100), 0, 100);
+  values.l3 = constrain(map(values.l3Raw, LIGHT_MIN, LIGHT_MAX, 0, 100), 0, 100);
+  values.max = max(values.l1, max(values.l2, values.l3));
+  return values;
+}
+
+int readMoisture()
+{
+  int value = analogRead(MOISTURE_SENSOR_PIN);
+  return constrain(map(value, MOISTURE_MIN, MOISTURE_MAX, 0, 100), 0, 100);
+}
+
+int readWaterPin(int pin)
+{
+  analogWrite(WATER_L1_PIN, 0);
+  analogWrite(WATER_L2_PIN, 0);
+  analogWrite(WATER_L3_PIN, 0);
+  analogWrite(WATER_L4_PIN, 0);
+  analogWrite(pin, 255);
+  delay(WATER_READING_TIME / 2);
+  int value = analogRead(WATER_SENSOR_PIN);
+  delay(WATER_READING_TIME / 2);
+  analogWrite(pin, 0);
+  return value;
+}
+
+int readWater()
+{
+  if (readWaterPin(WATER_L4_PIN) >= WATER_THRESHOLD)
+  {
+    return WATER_L4_VALUE;
+  }
+
+  if (readWaterPin(WATER_L3_PIN) >= WATER_THRESHOLD)
+  {
+    return WATER_L3_VALUE;
+  }
+
+  if (readWaterPin(WATER_L2_PIN) >= WATER_THRESHOLD)
+  {
+    return WATER_L2_VALUE;
+  }
+
+  if (readWaterPin(WATER_L1_PIN) >= WATER_THRESHOLD)
+  {
+    return WATER_L1_VALUE;
+  }
+
+  return 0;
+}
+
+// LIFECYCLE
+void normalMode()
+{
+  // I2C Init
+  Wire.begin();
+  sht.begin();
+
+  Serial.println("Measurements:");
+
+  int moisture = readMoisture();
+  Serial.print("Moisture: ");
+  Serial.println(moisture);
+
+  int water = readWater();
+  Serial.print("Water: ");
+  Serial.println(water);
+
+  Light light = readLight();
+  Serial.print("Light: ");
+  Serial.print(light.max);
+  Serial.print(" (");
+  Serial.print(light.l1);
+  Serial.print(", ");
+  Serial.print(light.l2);
+  Serial.print(", ");
+  Serial.print(light.l3);
+  Serial.print(") (");
+  Serial.print(light.l1Raw);
+  Serial.print(", ");
+  Serial.print(light.l2Raw);
+  Serial.print(", ");
+  Serial.print(light.l3Raw);
+  Serial.println(")");
+
+  HumAndTemp humAndTemp = readHumAndTemp();
+  Serial.print("Humidity: ");
+  Serial.println(humAndTemp.humidity);
+  Serial.print("Temperature: ");
+  Serial.println(humAndTemp.temperature);
+}
+
+void pairingMode()
+{
+  Serial.println("Pairing Mode");
+  delay(1000);
+}
+
 void setup()
 {
   // Determining WakeUp Cause
-  int buttonWakeUp = esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT1;
+  buttonWakeUp = esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT1;
 
   // Serial Init
   Serial.begin(9600);
@@ -52,19 +216,18 @@ void setup()
   }
   else
   {
+    timerWakeUpCount++;
     analogWrite(USER_LED_PIN, 0);
   }
 
   // Pairing Mode Check
   if (isButtonHeld(USER_BUTTON_PIN, PAIRING_MODE_HOLD_TIME) == 1)
   {
-    Serial.println("Pairing Mode");
-    delay(1000);
+    pairingMode();
   }
   else
   {
-    Serial.println("Default Mode");
-    delay(1000);
+    normalMode();
   }
 
   Serial.flush();
