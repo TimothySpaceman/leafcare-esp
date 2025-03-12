e1961958c234eced4743721f84ad8c8cf498401f
Timothy Rehenel <timothy.rehenel@gmail.com>
2025-03-12 14:20:34 +0200

    lifecycle skeleton

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # LeafCare Pot FirmWare
 
-Coming Soon
+### Current state: Skeleton
+
+At this moment, the firmware implements lifecycle skeleton:
+1. ESP checks wakeup reason (timer/button)
+2. If the wakeup is caused by the button - LED turns on
+3. If the button is held long enough - ESP switches to pairing mode, or in default mode otherwise
+4. ESP goes into the deep sleep until a certain time or the button is pressed
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,12 +1,80 @@
+// UTILS
+#define MILLIS_TO_MICROS_FACTOR 1000
+#define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)
+
+// DEFAULT VALUES
+#define DEFAULT_SLEEP_INTERVAL 5000 // Time interval (ms) for ESP wake-up
+
+// PAIRING MODE
+#define PAIRING_MODE_HOLD_TIME 4000 // Time (ms) for the User Button needs to be held to go into Pairing Mode
+
+// USER INTERFACE
+#define USER_BUTTON_PIN 25 // User Interface Button Pin
+#define USER_LED_PIN 15    // User Interface LED Pin
+
+// GLOBALS
+RTC_DATA_ATTR int wakeUpCount = 0;
+
+int isButtonHeld(int pin, int time)
+{
+  int start = millis();
+
+  while (true)
+  {
+    int state = digitalRead(pin);
+    int moment = millis();
+
+    if (state == LOW && moment - start < time)
+    {
+      return 0;
+    }
+
+    if (moment - start >= time)
+    {
+      return 1;
+    }
+  }
+}
+
 void setup()
 {
+  // Determining WakeUp Cause
+  int buttonWakeUp = esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT1;
+
+  // Serial Init
   Serial.begin(9600);
-  delay(1000);
-  Serial.println("Setup");
+
+  // User Interface Init
+  pinMode(USER_BUTTON_PIN, INPUT);
+  if (buttonWakeUp == 1)
+  {
+    analogWrite(USER_LED_PIN, 255);
+  }
+  else
+  {
+    analogWrite(USER_LED_PIN, 0);
+  }
+
+  // Pairing Mode Check
+  if (isButtonHeld(USER_BUTTON_PIN, PAIRING_MODE_HOLD_TIME) == 1)
+  {
+    Serial.println("Pairing Mode");
+    delay(1000);
+  }
+  else
+  {
+    Serial.println("Default Mode");
+    delay(1000);
+  }
+
+  Serial.flush();
+
+  // Sleep Setup
+  esp_sleep_enable_ext1_wakeup_io(BUTTON_PIN_BITMASK(USER_BUTTON_PIN), ESP_EXT1_WAKEUP_ANY_HIGH);
+  esp_sleep_enable_timer_wakeup(DEFAULT_SLEEP_INTERVAL * MILLIS_TO_MICROS_FACTOR);
+  esp_deep_sleep_start();
 }
 
 void loop()
 {
-  Serial.println("Loop");
-  delay(1000);
 }
